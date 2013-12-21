d41399267400d6c93aca7f9c56093b6d20f35649
Michael Brown <michael.brown6@my.jcu.edu.au>
2013-12-22 00:05:58 +1000

    Initial commit
    No features exists yet! None!!

---
+++ Program.cs
@@ -0,0 +1,32 @@
+/*
+ * Generic Roguelike
+ * A silly game of monsters and mayhem and... *ahem*, coding mistakes.
+ * Copyright 2013 by Michael J. Brown
+ * 
+ * This file is part of Generic Roguelike.
+ * Generic Roguelike is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Generic Roguelike is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with Generic Roguelike.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using System;
+
+namespace GenericRoguelike
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Hello World!");
+		}
+	}
+}
---
+++ README.md
@@ -0,0 +1,27 @@
+Generic Roguelike
+=================
+
+A silly game of monsters and mayhem and... *ahem*, coding mistakes.
+Copyright 2013 by Michael J. Brown
+
+About
+-----
+
+Truth be told I'm only doing this to teach myself C# and Mono!
+
+License
+-------
+
+This file is part of Generic Roguelike.
+Generic Roguelike is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Generic Roguelike is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Generic Roguelike.  If not, see <http://www.gnu.org/licenses/>.
